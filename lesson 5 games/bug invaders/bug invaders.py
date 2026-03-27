5a5d8518112a399b7558952988d81b38c2ed5f67
toaster0101 <henry.bray.star@gmail.com>
2026-03-27 17:02:10 +0000

    Add files via upload

--- lesson 5 games/bug invaders/bug invaders.py
+++ lesson 5 games/bug invaders/bug invaders.py
@@ -9,7 +9,12 @@ bugList=[]
 y=40
 x=40
 down=False
-for i in range(3):
+direction=1
+bugRemoveList=[]
+bulletRemoveList=[]
+g=0
+gameover=False
+for i  in range(3):
     for j in range(6):
         bug=Actor("bug")
         bug.pos=(x,y)
@@ -24,29 +29,53 @@ def draw():
         i.draw()
     for i in bulletList:
         i.draw()
+    if gameover:
+        screen.draw.text("End Of Game",(0,0),color="white",fontsize=20)
 def update():
-    global down
-    down=False
-    if ship.x<560:
+    global direction, g, gameover
+    for i in bugRemoveList:
+        if i in bugList:
+            bugList.remove(i)
+    for i in bulletRemoveList:
+        if i in bulletList:
+            bulletList.remove(i)
+    if ship.x<760:
         if keyboard.d:
             ship.x+=2
     if ship.x>40:
         if keyboard.a:
             ship.x-=2
     for i in bulletList:
-        i.y-=2
+        i.y-=4
         if i.y<0:
             bulletList.remove(i)
-    if bugList[-1].x<720:
+        for j in bugList:
+            if i.colliderect(j):
+                bugRemoveList.append(j)
+                bulletRemoveList.append(i)
+    if len(bugList)>0:
         for i in bugList:
-            i.x+=2
-    elif bugList[-1].x>710:
-        down=True
+            i.x+=1*direction
+        for j in bugList:
+            if bugList[g].x>760:
+                direction=-1
+                for i in bugList:
+                    i.y+=40
+            if bugList[g].x<40:
+                direction=1
+                for i in bugList:
+                    i.y+=40
+            g+=1
+            if g>=len(bugList):
+                g=0
+    for i in bugList:
+        if ship.colliderect(i):
+            gameover=True
 def on_key_down(key):
     if key==keys.SPACE:
         bullet=Actor("bullet")
         bullet.pos=(ship.x-1,ship.y-35)
-        if len(bulletList)<10:
+        if len(bulletList)<3:
             bulletList.append(bullet)
         else:
             bulletList.pop(0)
